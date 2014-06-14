dee44afdca0e6d3e48ed4bd3ba9ee28241632cef
Amir <Amir_Borzoei@yahoo.com>
2014-06-14 13:49:07 +0430

    AddSomeFilesToMiscFolder

---
+++ Misc/PermissionValueCellTemplateSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Grid;
+using DrArchiv.Proxy.Contracts;
+using DrArchiv.Shared.Models;
+
+namespace DrArchiv.Shared.Helper
+{
+    public class PermissionValueCellTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (System.ComponentModel.DesignerProperties.IsInDesignTool)
+                return null;
+
+            var rowData = ((GridCellData)item).RowData;
+            var presenter = (FrameworkElement)container;
+            var itemValue = rowData.Row as UserRolePermissionKey;
+
+            if (itemValue == null) return null;
+
+            switch (itemValue.PermissionKey.ValueType)
+            {
+                case permissionValueTypeEnum.Type_Boolean:
+                    return (DataTemplate)presenter.FindResource("editorBool");
+
+                case permissionValueTypeEnum.Type_Integer:
+                    return (DataTemplate)presenter.FindResource("editorInt");
+
+                case permissionValueTypeEnum.Type_Text:
+                    return (DataTemplate)presenter.FindResource("editorString");
+
+                case permissionValueTypeEnum.Type_Non:
+                    return (DataTemplate)presenter.FindResource("editorNone");
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Misc/TrackableCollection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Caliburn.PresentationFramework;
+
+namespace DrArchiv.Shared.Models
+{
+    public class TrackableCollection<T> : BindableCollection<T> where T : BaseModel
+    {
+        public TrackableCollection()
+        {
+            DeletedItems = new List<T>();
+        }
+
+        public TrackableCollection(IEnumerable<T> collection)
+            : base(CheckCollection(collection))
+        {
+            DeletedItems = new List<T>();
+        }
+
+        private static IEnumerable<T> CheckCollection(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                return new List<T>();
+
+            return collection;
+        }
+
+        public virtual IList<T> DeletedItems
+        {
+            get;
+            private set;
+        }
+
+        protected override void ClearItemsBase()
+        {
+            base.ClearItemsBase();
+            DeletedItems.Clear();
+        }
+
+        protected override void RemoveItemBase(int index)
+        {
+            var item = this[index];
+
+            base.RemoveItemBase(index);
+
+            if (item.State != ObjectStates.Added)
+            {
+                item.State = ObjectStates.Deleted;
+                DeletedItems.Add(item);
+            }
+        }
+
+        public bool RemovePermanent(T item)
+        {
+            var index = IndexOf(item);
+            if (index < 0)
+                return false;
+
+            base.RemoveItemBase(index);
+
+            return true;
+        }
+    }
+}
