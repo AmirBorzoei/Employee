82ac1159ec6c94c838f67b6957ce8abbb5d64325
Amir <Amir_Borzoei@yahoo.com>
2014-07-03 15:07:11 +0430

    Print_1

--- Employee/Employee/Employee.Personally/Views/EmployeeInfoView.xaml.cs
+++ Employee/Employee/Employee.Personally/Views/EmployeeInfoView.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using DevExpress.Xpf.Core.Native;
+using DevExpress.Xpf.Printing;
+using DevExpress.XtraPrinting;
 using Employees.Shared.Interfaces;
+using PaperKind = System.Drawing.Printing.PaperKind;
 
 namespace Employees.Personally.Views
 {
@@ -21,8 +25,28 @@ namespace Employees.Personally.Views
 
         public void Print(Window ownerWindow)
         {
-            
-            TableViewAllEmployees.ShowPrintPreviewDialog(ownerWindow);
+            //TableViewAllEmployees.ShowPrintPreviewDialog(ownerWindow);
+
+            using (var print = new PrintableControlLink(TableViewAllEmployees))
+            {
+                print.PrintingSystem.PageSettings.PaperKind = (PaperKind)DevExpress.Xpf.Core.Native.PaperKind.A4;
+                var preview = new DocumentPreviewWindow
+                {
+                    Owner = ownerWindow,
+                    Model = new LinkPreviewModel(print)
+                };
+
+                print.PrintingSystem.PageSettings.Landscape = true;
+                print.CreateDocument(true);
+                ((IPrintingSystem)print.PrintingSystem).AutoFitToPagesWidth = 1;
+                print.PrintingSystem.ShowPrintStatusDialog = true;
+
+
+                preview.FlowDirection = FlowDirection.RightToLeft;
+
+
+                preview.Show();
+            }
         }
     }
 }
